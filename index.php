eafb4a1c924dbfbce82f7a20f348136c799364eb
Manunirmal03 <134664437+Manunirmal03@users.noreply.github.com>
2025-07-29 17:23:47 +0530

    admin

--- index.php
+++ index.php
@@ -1,29 +1,30 @@
 <?php include ('head.php');?>
+
 <body>
+
     <div class="container">
         <div class="row">
-        <center><h1><b>C_ELECTZ</b></h1></center>
-		
-		<center><h3>College Voting Sytem</h3></center>
+
+        <center><h1><b>C_ELECTZ</b></h1></center>    
+
+		<center><h3>College Voting Sytem - Admin Panel</h3></center>
             <div class="col-md-4 col-md-offset-4">
-			
                 <div class="login-panel panel panel-default">
-				
                     <div class="panel-heading">
-                        <center><h3 class="panel-title"> Log In</h3></center>
+                        <center><h3 class="panel-title">Admin Log In</h3></center>
                     </div>
                     <div class="panel-body">
                         <form role="form" method = "post" enctype = "multipart/form-data">
                             <fieldset>
 							
                                 <div class="form-group">
-									<label for = "username" >ID No.</label>
-										<input class="form-control" placeholder="Please Enter Voter's ID Number" name="idno" type="text" required = "required" autofocus>
+									<label for = "username" >Username</label>
+										<input class="form-control" placeholder="Please Enter your Username" name="username" type="text" autofocus>
                                 </div>
 								
                                 <div class="form-group">
 									<label for = "username" >Password</label>
-										<input class="form-control" placeholder="Password" name="password" type="password" required = "required">
+										<input class="form-control" placeholder="Password" name="password" type="password" value="">
                                 </div>
                              
                               
@@ -34,10 +35,11 @@
 							
 									<?php include ('login_query.php');?>
                         </form>
-                        <h4><b>Note:</b> <i>One voter can only vote/login one time!!</i> </h4>
                     </div>
                 </div>
             </div>
+			
+			 </div>
         </div>
     </div>
 
